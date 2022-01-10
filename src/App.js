3a8ad43a4b9b33bcd62e13bca54e3eee6fc0483a
Dylan Law <dlaw96@hotmail.com>
2022-01-09 19:14:39 -0500

    Added filter, refactored code

--- src/App.js
+++ src/App.js
@@ -1,14 +1,56 @@
-import React from 'react';
+import React, {useState, useEffect} from 'react';
+import axios from 'axios';
 import Pagination from './components/Pagination';
-import './App.css';
+import {API_KEY} from './utils/config'
 
 const App = () => {
+  const [offset, setOffset] = useState(0);
+  const [data, setData] = useState([]);
+  const [perPage] = useState(10);
+  const [pageCount, setPageCount] = useState(0);
+  const [search, setSearch] = useState('');
 
-  return (
-    <div className="App">
-      <Pagination/>
-    </div>
-  );
+  useEffect(() => {
+    // use later
+    // http://api.marketstack.com/v1/tickers?access_key=${API_KEY}
+    axios.get('http://localhost:3001/data')
+    .then(response => {
+      const data = response.data;
+      const slice = data.slice(offset, offset + perPage)
+      setData(slice)
+      setPageCount(Math.ceil(data.length / perPage))
+    })
+    .catch(error => {
+      console.log(error)
+    })
+  }, [offset, perPage])
+
+  const handlePageClick = e => {
+    const selectedPage = e.selected;
+    console.log('selectedPage', selectedPage)
+    setOffset(selectedPage + 1)
+};
+
+const handleChange = e => {
+  setSearch(e.target.value);
+  console.log(e.target.value)
 }
 
-export default App;
+const filteredTickers = data.filter(dataFiltered => dataFiltered.name.toLowerCase().includes(search.toLowerCase()))
+
+
+  return(
+    <div className="App">
+      <div className="ticker-search">
+        <h1>Search a Ticker!</h1>
+        <input className="ticker-input" placeholder="search" onChange={handleChange}/>
+      </div>
+     <Pagination data={data} pageCount={pageCount} offset={offset} handlePageClick={handlePageClick} filteredTickers={filteredTickers}/>
+    </div>
+  )
+};
+
+export default App
+
+
+
--- src/components/Pagination.js
+++ src/components/Pagination.js
@@ -1,52 +1,49 @@
-import React, {useState, useEffect} from 'react';
-import axios from 'axios';
+import React from 'react';
 import ReactPaginate from 'react-paginate';
-import {API_KEY} from '../utils/config'
 
-const Pagination = () => {
-  const [offset, setOffset] = useState(0);
-  const [data, setData] = useState([]);
-  const [perPage] = useState(10);
-  const [pageCount, setPageCount] = useState(0)
-
-  useEffect(() => {
-    axios.get(`https://finnhub.io/api/v1/stock/symbol?exchange=US&token=${API_KEY}`)
-    .then(response => {
-      const data = response.data;
-      const slice = data.slice(offset, offset + perPage)
-      setData(slice)
-      setPageCount(Math.ceil(data.length / perPage))
-    })
-    .catch(error => {
-      console.log(error)
-    })
-  }, [offset, perPage])
-  
-  const handlePageClick = (e) => {
-    const selectedPage = e.selected;
-    console.log('selectedPage', selectedPage)
-    setOffset(selectedPage + 1)
-};
-
+const Pagination = ({data, pageCount, offset, handlePageClick, filteredTickers}) => {
+  console.log(filteredTickers)
 
   return(
-    <div className="App">
-      {data.map(d=><p key={d.displaySymbol}>{d.symbol}</p>)}
-       <ReactPaginate
-                    previousLabel={"prev"}
-                    nextLabel={"next"}
-                    breakLabel={"..."}
-                    breakClassName={"break-me"}
-                    pageCount={pageCount}
-                    marginPagesDisplayed={2}
-                    pageRangeDisplayed={5}
-                    onPageChange={handlePageClick}
-                    containerClassName={"pagination"}
-                    subContainerClassName={"pages pagination"}
-                    activeClassName={"active"}/> 
-    </div>
+  <div>{filteredTickers.length > 0 ? (
+    <div className="ticker-container">
+      <div className="ticker-header">
+        <p>Company Name</p>
+        <p>Ticker</p>
+        <p>Stock Exchange</p>
+        <p>Country</p>
+      </div>
+      {filteredTickers.map(d=>
+        <div className="ticker-row" key={d.symbol}>
+          <div className="ticker">
+            <h3>{d.name}</h3>
+          </div>
+          <div className="ticker-symbol">
+            <h3>{d.symbol}</h3>
+          </div>
+          <div className="ticker-exchange">
+            <h3>{d.stock_exchange.name}</h3>
+          </div>
+          <div className="ticker-country">
+            <h3>{d.stock_exchange.country}</h3>
+          </div>
+        </div>)}
+      <ReactPaginate
+                previousLabel={"prev"}
+                nextLabel={"next"}
+                breakLabel={"..."}
+                breakClassName={"break-me"}
+                pageCount={pageCount}
+                marginPagesDisplayed={2}
+                pageRangeDisplayed={5}
+                onPageChange={handlePageClick}
+                containerClassName={"pagination"}
+                subContainerClassName={"pages pagination"}
+                activeClassName={"active"}/> 
+    </div> ) : (<div>No Ticker Found</div>)}
+  
+  </div>
   )
-
 };
 
-export default Pagination
+export default Pagination;
